d24384f257afb18e311f33dcf6e50b6a2dddc670
gajdos.csanad <20.gajdos.csanad@mechwart.com>
2024-10-09 09:14:20 +0200

    Nem működik

---
+++ ViccAdatbazis/Controllers/ViccekController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViccAdatbazis.Data;
+using ViccAdatbazis.Models;
+
+namespace ViccAdatbazis.Controllers
+{
+    public class ViccekController : Controller
+    {
+        [HttpGet]
+        [Route("[controller]")]
+        public string Get()
+        {
+            Vicc ujVicc = new();
+            ujVicc.Tartalom = " A Facebook olyan, mint a hűtőszekrény. 15 percenként kinyitogatod, és mindig ugyanaz van benne. ";
+            ujVicc.Feltolto = "Rasputin";
+            ujVicc.Tetszik = 764;
+            ujVicc.NemTetszik = 96;
+            ujVicc.Aktiv = true;
+
+            
+            return $"{ujVicc.Tartalom}\nFeltöltő: {ujVicc.Feltolto}\nLikeok: {ujVicc.Tetszik}\nDiszlájkok: {ujVicc.NemTetszik}";
+            
+        }
+    }
+}
--- ViccAdatbazis/Program.cs
+++ ViccAdatbazis/Program.cs
@@ -4,24 +4,29 @@ using ViccAdatbazis.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ViccDbContext>(
     options => options.UseSqlite(connectionString: @"Data Source = .\Data\ViccAdatbazis.db"));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.MapControllers();
 
 app.UseAuthorization();
 
